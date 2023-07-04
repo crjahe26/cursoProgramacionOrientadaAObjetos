735cc57f849a8a51b55969b489a219ceb7dfa61f
crjahe26 <crjaramillohe@unal.edu.co>
2023-07-04 18:06:28 -0500

    Prueba de la ejecución de las nuevas clases de PHP

--- cursoPOOUber/PHP/index.php
+++ cursoPOOUber/PHP/index.php
@@ -1,7 +1,10 @@
 <?php
 
 require_once('car.php');
+require_once('uberX.php');
+require_once('uberPool.php');
 require_once('account.php');
+
 
 $car = new Car("AW456", new Account("Andres Herrera", "AMS123"));
 $car->printDataCar();
@@ -9,4 +12,10 @@ $car->printDataCar();
 $car2 = new Car("AW456", new Account("Andrea Herrera", "AND123"));
 $car2->printDataCar();
 
+$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
+$uberX->PrintDataCar();
+
+
+$uberPool = new UberPool("ASD123", new Account("Andrea Ferran", "AND456"), "Dodge", "Attitude");
+$uberPool->PrintDataCar();
 ?>

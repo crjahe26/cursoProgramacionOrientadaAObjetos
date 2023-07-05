37686bf2111da0316a691b07f2b423b49b9f527c
crjahe26 <crjaramillohe@unal.edu.co>
2023-07-05 12:00:45 -0500

    Encapsulamiento en Java, restricción de asignación de cantidad de pasajeros

--- cursoPOOUber/Java/Car.java
+++ cursoPOOUber/Java/Car.java
@@ -2,7 +2,7 @@ public class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passegenger;
+    private Integer passegenger; /* Ponemos este modificador de acceso para que pueda ser modificado solo desde la misma clase */
 
     public Car(String license, Account driver){
         this.license = license;
@@ -10,6 +10,24 @@ public class Car {
     }
 
     void printDataCar() {
-        System.out.println("License: " + license + " Name Driver: " + driver.name);
+        if((license != null) && (driver.name != null) && (passegenger != null)) {
+            System.out.println("License: " + license + " Name Driver: " + driver.name + " Passengers: " + passegenger);
+        }
+        else {
+            System.out.println("Tienes valores nulos o invalidos en los atributos License, Name o Passengers");
+        }
+        
+    }
+
+    public Integer getPassegenger(){
+        return passegenger;
+    }
+
+    public void setPassegenger(Integer passenger){
+        if (passenger == 4) {
+            this.passegenger = passenger;   
+        }else{
+            System.out.println("Necesitas asignar 4 pasajeros");
+        }
     }
 }
--- cursoPOOUber/Java/Main.java
+++ cursoPOOUber/Java/Main.java
@@ -2,16 +2,17 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         
-        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
-        car.passegenger = 4;
-        car.printDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Sandero");
+        /* uberX.passegenger = 4; */
+        uberX.setPassegenger(2);
+        uberX.printDataCar();
 
-
+/* 
         Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA124"));
         car2.passegenger = 3;
         car2.printDataCar();
 
         Driver cond = new Driver("Pedro", "1548", 8555, "pedro11@gmail.com", "pedro2212");
-        System.out.println("No. Documento " + cond.document + " Nombre " + cond.name + " Id " + cond.id);
+        System.out.println("No. Documento " + cond.document + " Nombre " + cond.name + " Id " + cond.id); */
     }
 }
